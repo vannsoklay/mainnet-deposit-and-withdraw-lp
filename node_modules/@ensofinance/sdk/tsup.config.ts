8619977935faf6dd585e2891b8acc4114104ad05
vannsoklay <soklayvann@gmail.com>
2025-08-25 16:08:30 +0700

    first commit

--- node_modules/@ensofinance/sdk/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/index.ts"], // Specify the entry point of your SDK
-  format: ["cjs", "esm"], // Output formats: CommonJS and ES Module
-  dts: true, // Generate TypeScript declaration files
-  sourcemap: true, // Generate source maps for debugging
-  clean: true, // Clean the output directory before each build
-});
